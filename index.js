960220297eac713d5deb51e742517193f3a0ce0c
marohrdanz <marohrdanz@mdanderson.org>
2023-08-14 13:36:32 -0500

    Bind and unbind for each authenticate
    This is to address the ECONNRESET issue from ldapauth-fork. This
    solution is to bind and unbind from the ldap server for each
    authentication. Note: the /verify endpoint does NOT require any
    connection to ldap, only the /authenticate endpoint.
    
    Addresses issue #9

--- index.js
+++ index.js
@@ -29,48 +29,76 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(require('cors')());
 
-var auth = null;
 if (settings.hasOwnProperty( 'ldap' ) && settings.hasOwnProperty( 'jwt' )) {
 	logger.debug("LdapAuth settings: " + JSON.stringify(settings.ldap, ut.hideSecrets, 2));
 	logger.debug("Adding bunyan logger to LdapAuth settings");
 	settings.ldap['log'] = logger;
-	auth = new LdapAuth(settings.ldap);
+} else {
+	logger.error("LDAP and JWT settings are required. Exiting.");
+	process.exit(1);
 }
 
+
 if (settings.hasOwnProperty( 'jwt' )) {
-    if (!settings.jwt.hasOwnProperty('timeout')) {
-        settings.jwt.timeout = 1;
-        logger.warn("Settings.jwt.timeout not set - using default: " + settings.jwt.timeout);
-    }
-    if (!settings.jwt.hasOwnProperty('timeout_units')) {
-        settings.jwt.timeout_units = 'hour';
-        logger.warn("Settings.jwt.timeout_units not set - using default: " + settings.jwt.timeout_units);
-    }
-    logger.info('JWT tokens will expire after ' + settings.jwt.timeout + ' ' + settings.jwt.timeout_units);
-    app.set('jwtTokenSecret',
-            settings.jwt.base64 ? new Buffer(settings.jwt.secret, 'base64') : settings.jwt.secret);
+	if (!settings.jwt.hasOwnProperty('timeout')) {
+		settings.jwt.timeout = 1;
+		logger.warn("Settings.jwt.timeout not set - using default: " + settings.jwt.timeout);
+	}
+	if (!settings.jwt.hasOwnProperty('timeout_units')) {
+		settings.jwt.timeout_units = 'hour';
+		logger.warn("Settings.jwt.timeout_units not set - using default: " + settings.jwt.timeout_units);
+	}
+	logger.info('JWT tokens will expire after ' + settings.jwt.timeout + ' ' + settings.jwt.timeout_units);
+	app.set('jwtTokenSecret',
+		settings.jwt.base64 ? new Buffer(settings.jwt.secret, 'base64') : settings.jwt.secret);
+}
+
+/* Create a new ldap connection */
+let bind = function () {
+	try {
+		logger.debug("Creating new bind")
+		let auth = new LdapAuth(settings.ldap);
+		return auth;
+	} catch (err) {
+		logger.error("Error creating new bind: ", err);
+		return null;
+	}
+}
+
+/* Closing ldap connection */
+let unbind = function (auth) {
+	try {
+		logger.debug("Unbinding")
+		auth.close();
+	} catch (err) {
+		logger.error("Error unbinding: ", err);
+	}
 }
 
 var authenticate = function (username, password) {
+	let auth = bind();
 	logger.debug("Authenticating user: " + username);
 	return new Promise(function (resolve, reject) {
 		logger.debug("In authenticate promise");
 		auth.authenticate(username, password, function (err, user) {
-			logger.debug("In auth.authenticate callback");
-			if(err)
+			unbind(auth);
+			if(err) {
+				logger.error("Reject because of err: ", err);
 				reject(err);
-			else if (!user)
+			} else if (!user) {
+				logger.error("Reject because no user");
 				reject();
-			else
+			} else {
 				resolve(user);
+			}
 		});
 	});
 };
 
 app.post('/ldap-jwt/authenticate', function (req, res) {
-	if(auth && req.body.username && req.body.password) {
+	if(req.body.username && req.body.password) {
 		logger.debug(JSON.stringify(req.body, ut.hideSecrets, 2));
-		authenticate(req.body.username, req.body.password, req.body.authorized_groups)
+		authenticate(req.body.username, req.body.password)
 			.then(function(user) {
 				logger.debug("User authenticated");
 				if (req.body.authorized_groups != undefined) {
@@ -101,23 +129,14 @@ app.post('/ldap-jwt/authenticate', function (req, res) {
 				// Ldap reconnect config needs to be set to true to reliably
 				// land in this catch when the connection to the ldap server goes away.
 				// REF: https://github.com/vesse/node-ldapauth-fork/issues/23#issuecomment-154487871
-				logger.error(err);
-
+				logger.error("Error from authenticate promise: ", err);
 				if (err.name === 'InvalidCredentialsError' || (typeof err === 'string' && err.match(/no such user/i)) ) {
 					res.status(401).send({ error: 'Wrong user or password'});
 				} else if (err == "Server not configured for authorized_group verification" || err == "User not in authorized_groups") {
 					res.status(401).send({error: "User is not authorized"});
 				} else {
-					// ldapauth-fork or underlying connections may be in an unusable state.
-					// Reconnect option does re-establish the connections, but will not
-					// re-bind. Create a new instance of LdapAuth.
-					// REF: https://github.com/vesse/node-ldapauth-fork/issues/23
-					// REF: https://github.com/mcavage/node-ldapjs/issues/318
-
-					res.status(500).send({ error: 'Unexpected Error'});
-					auth = new LdapAuth(settings.ldap);
+					res.status(500).send({ error: 'Unexpected Error. Please try again.'});
 				}
-
 			});
 		} else {
 			logger.error("No username or password supplied");
