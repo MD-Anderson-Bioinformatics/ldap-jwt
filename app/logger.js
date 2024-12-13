29a9db682890c875de487099d0441973c38979c1
MARohrdanz <marohrdanz@mdanderson.org>
2024-12-13 09:14:38 -0600

    Run prettier on js files
    Contents of .prettierrc:
    
    {
      "printWidth": 100,
      "proseWrap": "never",
      "trailingComma": "none"
    }

--- app/logger.js
+++ app/logger.js
@@ -1,13 +1,12 @@
-const bunyan = require('bunyan');
-const bformat = require('bunyan-format');
-const formatOut = bformat({ outputMode: 'long', color: true });
+const bunyan = require("bunyan");
+const bformat = require("bunyan-format");
+const formatOut = bformat({ outputMode: "long", color: true });
 
-const logger = bunyan.createLogger({ // also used for logging in ldapauth-fork and ldapjs
-	name: 'ldap-jwt',
-	level: (process.env.LOG_LEVEL || 'info'),
-	stream: formatOut
+const logger = bunyan.createLogger({
+  // also used for logging in ldapauth-fork and ldapjs
+  name: "ldap-jwt",
+  level: process.env.LOG_LEVEL || "info",
+  stream: formatOut
 });
 
 module.exports = logger;
-
-
